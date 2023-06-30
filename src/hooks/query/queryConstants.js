44e174f0ddb3cda84926bf49d8f87eeb4faf86b0
GloryDream413 <putnikdeveloperbg@gmail.com>
2023-06-30 14:55:18 +0900

    frontEnd updated.

--- src/hooks/query/queryConstants.js
+++ src/hooks/query/queryConstants.js
@@ -6,4 +6,5 @@ export const authorizedUsers = [
   '0x40426F16818A5b8ce1e4a2ad58279cF309f356cc'
 ]
 
-export const endPointOfApi = 'https://famaone.xyz/api/'
+//export const endPointOfApi = 'https://famaone.xyz/api/'
+export const endPointOfApi = 'http://localhost:5000/api'
